LineageOS/android_device_motorola_surnia

[BoardConfig] Change kernel defconfig name
Change-Id: I1b8b0287b2266b7fb57b5e91cdd8a11e4652c826

BoardConfig.mk
@@ -47,7 +47,7 @@ BOARD_KERNEL_SEPARATED_DT := true
 BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8916
-TARGET_KERNEL_CONFIG := msm8916_defconfig
+TARGET_KERNEL_CONFIG := surnia_defconfig
 
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 

SlimRoms/device_motorola_surnia

Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

BoardConfig.mk
@@ -47,7 +47,7 @@ BOARD_KERNEL_SEPARATED_DT := true
 BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8916
-TARGET_KERNEL_CONFIG := slim_msm8916_defconfig
+TARGET_KERNEL_CONFIG := slim_surnia_defconfig
 
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 