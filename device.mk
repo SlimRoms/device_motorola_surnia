8d8334a2b5d1320ad4949a2740e6960226828179
Josue Rivera <prbassplayer@gmail.com>
2015-11-05 18:28:50 +0100

    surnia: Add boringssl wrapper hack
    Change-Id: I63a2e4beafdd4e0854613de37941c8087156aac1
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- device.mk
+++ device.mk
@@ -82,6 +82,10 @@ PRODUCT_COPY_FILES +=  \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# BoringSSL hack
+PRODUCT_PACKAGES += \
+    libboringssl-compat
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8916 \
