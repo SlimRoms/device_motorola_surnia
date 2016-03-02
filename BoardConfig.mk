7a28f8c892c7a1febae4d623986d5c67a84c6242
Josue Rivera <prbassplayer@gmail.com>
2016-03-02 19:05:56 +0100

    Surnia: Use default defconfig
    Change-Id: I8b45da24ad3025337447b3700c23972a9cd98ea9
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,7 +20,7 @@ include device/motorola/msm8916-common/BoardConfigCommon.mk
 
 LOCAL_PATH := device/motorola/surnia
 
-TARGET_KERNEL_CONFIG := slim_surnia_defconfig
+TARGET_KERNEL_CONFIG := surnia_defconfig
 
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := XT1514,XT1521,XT1524,XT1526,XT1527,XT1523,surnia_uds,surnia_umts,surnia,surnia_udstv
