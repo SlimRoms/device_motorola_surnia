1ee6b5e5de35e47f100ae729f684e7208336578a
Josue Rivera <prbassplayer@gmail.com>
2017-09-10 11:33:44 -0400

    surnia: Init: Update for O
    Change-Id: Ie127855d6e69a3193072111efc51ce874ee38231
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- init/Android.mk
+++ init/Android.mk
@@ -7,5 +7,6 @@ LOCAL_C_INCLUDES := system/core/init
 LOCAL_CPPFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_surnia.cpp
 LOCAL_MODULE := libinit_surnia
+LOCAL_STATIC_LIBRARIES := libbase
 
 include $(BUILD_STATIC_LIBRARY)
